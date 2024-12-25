fdee480074f08ba573c49e47053911e79e6cce5d
Akshay-Tarde <akshaymtarde@gmail.com>
2024-12-25 18:52:18 +0530

    Created and styled the Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Header() {
+    return (
+        <header>
+            <div className="img-div"><img src="chef-claude-icon.png" alt="Chef Claude Icon" /></div>
+            <h1>Chef Claude</h1>
+        </header>
+    )
+}
