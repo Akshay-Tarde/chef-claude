b82db32bed89f578cf6a245e7b0ec9fc9a4172ee
Akshay-Tarde <akshaymtarde@gmail.com>
2024-12-25 19:22:39 +0530

    Created and styled the form component

---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+export default function Main() {
+    return (
+        <main>
+            <form action="submit">
+                <input type="text" placeholder="e.g.Oregano" />
+                <button>+ Add ingredient</button>
+            </form>
+        </main>
+    )
+}
