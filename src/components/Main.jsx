6e0adaa2bc8aa865392ce53e07a9026746b9c138
Akshay-Tarde <akshaymtarde@gmail.com>
2024-12-27 18:23:20 +0530

    Added State to the form component.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,29 @@
+import React from 'react';
 export default function Main() {
+
+    const [ingredients, setIngredients] = React.useState(["Chicken"])
+
+    const ingredientsList = ingredients.map( item => <li>{item}</li>)
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        setIngredients( prevState => [...prevState, newIngredient])
+    }
     return (
         <main>
-            <form action="submit">
-                <input type="text" placeholder="e.g.Oregano" />
+            <form onSubmit={handleSubmit} className="add-ingredient-form" action="submit">
+                <input 
+                    type="text" 
+                    placeholder="e.g.Oregano"
+                    aria-label="Add ingredient"
+                    name="ingredient" />
                 <button>+ Add ingredient</button>
             </form>
+            <ul>
+                {ingredientsList}
+            </ul>
         </main>
     )
 }
