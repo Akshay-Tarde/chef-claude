291fb2011db8d3de46de888de56fbe046fbefa5a
Akshay-Tarde <akshaymtarde@gmail.com>
2025-01-01 17:44:58 +0530

    Installed React 19 and refactored Form by using action

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
 export default function Main() {
 
-    const [ingredients, setIngredients] = React.useState([])
+    const [ingredients, setIngredients] = React.useState(["oregano"])
 
     const ingredientsList = ingredients.map( item => <li>{item}</li>)
 
-    function handleSubmit(event) {
-        event.preventDefault()
-        const formData = new FormData(event.currentTarget)
+    function addIngredient(formData) {
+        // event.preventDefault()
         const newIngredient = formData.get("ingredient")
         setIngredients( prevState => [...prevState, newIngredient])
     }
+
     return (
         <main>
-            <form onSubmit={handleSubmit} className="add-ingredient-form" action="submit">
+            <form action={addIngredient} className="add-ingredient-form">
                 <input 
                     type="text" 
-                    placeholder="e.g.Oregano"
+                    placeholder="e.g.oregano"
                     aria-label="Add ingredient"
                     name="ingredient" />
-                <button>+ Add ingredient</button>
+                <button >+ Add ingredient</button>
             </form>
             <ul>
                 {ingredientsList}
             </ul>
         </main>
-    )
+    );
 }
