99396c043a32097268b6f15e3a9d3f29c650b99f
Akshay-Tarde <akshaymtarde@gmail.com>
2025-01-02 10:09:30 +0530

    Added conditional rendering logic to ingredients list and get recipe containers

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -21,17 +21,17 @@ export default function Main() {
                     name="ingredient" />
                 <button >+ Add ingredient</button>
             </form>
-            <section>
+            { ingredients.length > 0 ? <section>
                 <h2>Ingredients on hand:</h2>
                 <ul className='ingredients-list'>{ingredientsList}</ul>
-                <div className="get-recipe-container">
+               { ingredients.length > 3 && <div className="get-recipe-container">
                     <div>
                         <h3>Ready for a recipe?</h3>
                         <p>Generate a recipe from your list of ingredients</p>
                     </div>
                     <button>Get a recipe</button>
-                </div>
-            </section>
+                </div>}
+            </section> : null}
         </main>
     );
 }
